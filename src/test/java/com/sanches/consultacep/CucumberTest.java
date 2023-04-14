73cfb9c46895b5109a2adf65904f34e0e8c419eb
marcosvinicius <marcossanches710@gmail.com>
2023-04-13 21:18:13 -0300

    Implementando mais do teste cucumber na classe CucumberTest.java

--- src/test/java/com/sanches/consultacep/CucumberTest.java
+++ src/test/java/com/sanches/consultacep/CucumberTest.java
@@ -1,4 +1,4 @@
-package com.sanches.consultacep.stepDefinitions;
+package com.sanches.consultacep;
 
 import com.sanches.consultacep.controller.CepController;
 import com.sanches.consultacep.controller.request.CepRequest;
@@ -7,22 +7,15 @@ import com.sanches.consultacep.exception.BadRequestException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.Assert.assertEquals;
 @CucumberContextConfiguration
-@RunWith(Cucumber.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@CucumberOptions(features = "cep.feature:buscaCep.feature")
-public class BuscaCepSteps {
+public class CucumberTest {
 
     private CepRequest cepRequest;
     private ResponseEntity<?> responseEntity;
@@ -30,26 +23,46 @@ public class BuscaCepSteps {
     private CepController cepController;
 
     @Autowired
-    public BuscaCepSteps(CepController cepController) {
+    public CucumberTest(CepController cepController) {
         this.cepController = cepController;
     }
 
-    @Given("um CEP válido")
+    @Given("usuário informou um CEP válido")
     public void umCepValido() {
         cepRequest = new CepRequest("15138226");
     }
 
-    @When("o serviço de busca de CEP é chamado")
+    @Given("usuário informou um CEP inválido")
+    public void que_o_usuário_informou_um_CEP_inválido() {
+        cepRequest = new CepRequest("151382-26");
+    }
+
+    @When("faço uma requisição para buscar o CEP")
     public void buscaCep() throws BadRequestException {
         responseEntity = cepController.buscaCep(cepRequest);
     }
 
-    @Then("a resposta deve conter o CEP e suas informações correspondentes")
+    @Then("a resposta deve ter o status {int} Ok")
+    public void a_resposta_deve_ter_o_status_200_OK() {
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Then("a resposta deve ter o status 400 Bad Request")
+    public void a_resposta_deve_ter_o_status_400_Bad_Request() {
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Then("o corpo da resposta deve conter um CepResponse válido")
     public void respostaContemCepEInformacoes() {
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isInstanceOf(CepResponse.class);
         CepResponse cepResponse = (CepResponse) responseEntity.getBody();
         assertThat(cepResponse.getCep()).isEqualTo(cepRequest.getCep());
         assertThat(cepResponse.getCep()).isNotEmpty();
+        assertThat(cepResponse.getValorFrete().equals(7.85));
+        assertThat(cepResponse.getBairro().equals("Jardim Santa Cláudia"));
+        assertThat(cepResponse.getEstado().equals("SP"));
+        assertThat(cepResponse.getRua().equals("Rua Domingos Tedeschi"));
+        assertThat(cepResponse.getComplemento().equals(""));
     }
 }
