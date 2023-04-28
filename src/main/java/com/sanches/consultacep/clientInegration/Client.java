a2a7f1cf3c6a0c9512eae5bddcf133014d295c16
marcosvinicius <marcossanches710@gmail.com>
2023-04-28 18:05:12 -0300

    Retirando os imports que não estavam sendo utilizados, na classe clint

--- src/main/java/com/sanches/consultacep/clientInegration/Client.java
+++ src/main/java/com/sanches/consultacep/clientInegration/Client.java
@@ -1,17 +1,15 @@
 package com.sanches.consultacep.clientInegration;
 
-
 import com.sanches.consultacep.basePath.BasePath;
 import com.sanches.consultacep.controller.response.ReturnIntegrationResponse;
 import com.sanches.consultacep.exception.BadRequestException;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "consulta-cep", url = "${feign.client.config.consulta-cep.url}")
-public interface Client {
+public interface  Client {
     @GetMapping(value = BasePath.BASE_PATH_INTEGRATION_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
     ReturnIntegrationResponse consultaCep(
             @PathVariable("cep") String cep)throws BadRequestException;
