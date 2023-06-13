6553d0c816670a82dd6328ab0c67deb9ce7a5f79
marcosvinicius <marcossanches710@gmail.com>
2023-06-13 18:41:22 -0300

    Corrigindo o bug de nullPointerException, e retornando a mensagem de erro, e deixando o log menos poluido quando o cep não é identificado

--- src/main/java/com/sanches/consultacep/utils/Constants.java
+++ src/main/java/com/sanches/consultacep/utils/Constants.java
@@ -6,11 +6,10 @@ import lombok.experimental.UtilityClass;
 public class Constants {
     public final String CEP_INVALIDO = "CEP invalido, verifique se os dados foram inseridos corretamente";
     public final String CEP_INVALIDO_ADICIONE_APENAS_NUMEROS = "CEP invalido, adicione apenas valores numericos";
-    public final String CEP_INEXISTENTE = "Estado não encontrado. Favor verifique se o cep informado esta correto";
+    public final String CEP_INEXISTENTE = "Estado nao encontrado. Favor verifique se o cep informado esta correto";
     public final String SWAGGER_PACKAGE = "com.sanches.consultacep.controller";
     public final String SWAGGER_DESCRIPTION_TAG = "Integração com a API para a busca de Cep";
     public final String SWAGGER_TITLE_API = "CONSULTA CEP";
     public final String SWAGGER_API_DESCRIPTION_APRESENTATION = "Teste para vaga de dev Java WIPRO";
     public final String SWAGGER_API_VERSION = "1,0,1";
-
 }
